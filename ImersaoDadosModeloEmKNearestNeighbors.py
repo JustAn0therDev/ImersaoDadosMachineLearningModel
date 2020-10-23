95402ad102d5c73e22bf5eef0d0f6a6b7791345f
JustAn0therDev <ruanmontelo@gmail.com>
2020-10-23 17:24:49 -0300

    Adicionado "split" dos dados de teste e treino e conversão de estrutura de dados "array" do numpy para listas nativas em Python

--- ImersaoDadosModeloEmKNearestNeighbors.py
+++ ImersaoDadosModeloEmKNearestNeighbors.py
@@ -17,6 +17,11 @@ grades_from_people_who_actually_did_the_test = data.query('NU_NOTA_TOTAL != 0')[
 exit_math_test_data = grades_from_people_who_actually_did_the_test.dropna()['NU_NOTA_MT']
 entry_data = grades_from_people_who_actually_did_the_test.dropna()[['NU_NOTA_CH', 'NU_NOTA_LC', 'NU_NOTA_CN', 'NU_NOTA_REDACAO']]
 
+# Seed para evitar resultados com maior aleatoriedade.
+SEED = 4321
+
+x_train, x_test, y_train, y_test = train_test_split(entry_data, exit_math_test_data, test_size=0.25, random_state=SEED)
+
 """
     Imaginando um scatterplot, escolhemos um ponto no gráfico e olhamos
     para os 1000 "vizinhos" mais próximos daquele ponto para calcular a média entre eles
@@ -25,11 +30,12 @@ entry_data = grades_from_people_who_actually_did_the_test.dropna()[['NU_NOTA_CH'
 neighbor_regressor = KNeighborsRegressor(n_neighbors=1000)
 neighbor_regressor.fit(x_train, y_train)
 
-neighbor_regressor_predictions = neighbor_regressor.predict(x_test)
+neighbor_regressor_predictions = list(neighbor_regressor.predict(x_test))
+y_test_list = list(y_test)
 
 dataframe_diff = pd.DataFrame()
 dataframe_diff['Actual'] = y_test_list
-dataframe_diff['Prediction'] = list(neighbor_regressor_predictions)
+dataframe_diff['Prediction'] = neighbor_regressor_predictions
 dataframe_diff['Difference'] = (dataframe_diff['Actual'] - dataframe_diff['Prediction'])
 dataframe_diff['Squared_Difference'] = (dataframe_diff['Prediction'] - dataframe_diff['Prediction'])**2
 
